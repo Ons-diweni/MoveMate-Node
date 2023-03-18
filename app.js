4be9e4992c3b5941651fc4fb86578dc9af71820c
Ons <ons.diweni@esprit.tn>
2023-03-18 15:11:41 +0100

    database connection

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 require('dotenv').config();
 const express = require ('express')
 const app = express ()
+const mongoose = require('mongoose')
 
 
+mongoose.connect(process.env.URL,{ useNewUrlParser: true, useUnifiedTopology: true })
+.then(()=> {console.log('Successfully connected to the database')})
+.catch((err)=>{ console.log('Error connecting to the database',err)})
 
 
 
