25aa25349d7a449d0a850986860339d00e33547f
Ons <ons.diweni@esprit.tn>
2023-03-28 02:31:23 +0100

    resgistration endpoint

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,31 +6,36 @@ const asyncHandler = require ('express-async-handler');
 
 
 
-//          registration
-exports.register = asyncHandler (async (req, res) => {
-  const {username, password, email, cin} = req.body;
-  if (!cin || !password || !email) {
-    res.status (400); //Bad request
-    return res.json ({error: 'Required fields are missing'});
+//              registration
+exports.register = asyncHandler(async (req, res) => {
+  try {
+    const {username, password, email, cin} = req.body;
+    if (!cin || !password || !email) {
+      res.status (400); //Bad request
+      return res.json ({error: 'Required fields are missing'});
+    }
+    //check if user exists
+    const userFetched = await User.findOne ({cin});
+    if (userFetched) {
+      res.status (400);
+      return res.json ({error: 'User already exists'});
+    }
+    //Hash user Password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password , salt)
+
+    //create and persist the user in the database
+    const newUser = await User.create({username , email , password : hashedPassword , cin})
+
+    if(newUser) {
+      res.status(201).json(newUser)
+    } else {
+      res.status(400).json({error : 'Error while persisting user in the database'})
+    }
+  } catch (err) {
+    // Handle any unexpected errors here
+    res.status(500).json({error: 'Unexpected error occurred while registering user'})
   }
-  //check if user exists
-  const userFetched = await User.findOne ({cin});
-  if (userFetched) {
-    res.status (400);
-    return res.json ({error: 'User already exists'});
-  }
-  //Hash user Password
-  const salt = await bcrypt.genSalt(10)
-  const hashedPassword = await bcrypt.hash(password , salt)
-
-  //create and persist the user in the database
-  const newUser = await User.create({username , email , password : hashedPassword , cin})
-
-  if(newUser) {res.status(201).json(newUser)} 
-  else res.status(400).json({error : 'Error while persisting user in the database'})
-
-
-
 });
 
 
